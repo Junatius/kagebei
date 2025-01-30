645e698bf36320c75d5b9352aa12778022805a10
Junatius <cjuniar0@gmail.com>
2025-01-30 21:28:42 +0800

    fix: adjustment for data

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -20,49 +20,20 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-
         // Create Gereja instances
-        Gereja::factory(5)->create(); 
-        echo "Gereja instances created\n";
+        $gereja = Gereja::factory()->create([
+            'nama' => 'Gereja Baptis Imanuel Indonesia',
+            'alamat' => 'Jl. Minahasa - Tompaso, Tempok, Kec. Tompaso, Kabupaten Minahasa, Sulawesi Utara 95693',
+            'kontak' => '082293967401',
+        ]);
 
-        $gereja = Gereja::inRandomOrder()->first();
-
-        // Create Pengguna instances
         User::factory()->create([
             'id_gereja' => $gereja->id,
-            'name' => 'Admin User',
+            'name' => 'admin',
             'email' => 'admin@gmail.com',
             'role' => 'admin',
             'password' => Hash::make('password123'),
             'remember_token' => Str::random(10),
         ]);
-        User::factory()->create([
-            'id_gereja' => $gereja->id,
-            'name' => 'User',
-            'email' => 'user@gmail.com',
-            'role' => 'user',
-            'password' => Hash::make('password123'),
-            'remember_token' => Str::random(10),
-        ]);
-
-        User::factory(10)->create();
-        echo "Pengguna instances created\n";
-
-        // Create Keanggotaan instances (with relationships)
-        $chosenGerejaId = Gereja::inRandomOrder()->value('id');
-        // Create Keanggotaan instances (with relationships)
-        Keanggotaan::factory(127)->create([
-            'id_gereja' => $chosenGerejaId,
-        ]);
-        echo "Keanggotaan instances created\n";
-
-        // Create Transaksi instances (with relationships), using the same Gereja ID
-        Transaksi::factory(163)->create([
-            'id_gereja' => $chosenGerejaId, // Use the same $chosenGerejaId
-        ]);
-        echo "Transaksi instances created\n";
-
-        Pengumuman::factory(15)->create();
-        echo "Pengumuman instances created\n";
     }
 }
