0a89de2ce0c0d6eeb8812ee62bdf6b91a556c83e
Junatius <cjuniar0@gmail.com>
2025-01-30 20:40:01 +0800

    feat: adjustment needed for deploy

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -17,6 +17,8 @@ return Application::configure(basePath: dirname(__DIR__))
             'role' => RoleMiddleware::class,
             'log' => LogRequests::class,
         ]);
+        
+        $middleware->trustProxies(at: '*');
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
